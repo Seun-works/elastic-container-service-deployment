2987f2f86ad08fe4bbac170a7e9d4643b1f66823
Seun Ogundipe <sogundipe@gmail.com>
2025-03-02 14:06:01 +0000

    No-Ticket: Ensured appropriate instance was used for ecs

--- auto_scaling_group.tf
+++ auto_scaling_group.tf
@@ -1,14 +1,22 @@
 # Created a launch template
 resource "aws_launch_template" "elastic_launch_template" {
-  name_prefix            = "elastic_container_template"
-  image_id               = data.aws_ami.ubuntu.id
-  instance_type          = "t3.micro"
-  key_name               = "ecslog"
-  user_data              = filebase64("${path.module}/user_data.sh")
+  name_prefix   = "elastic_container_template"
+  image_id      = data.aws_ssm_parameter.ecs_node_ami.value # Used an ecs optimized image from the AWS marketplace
+  instance_type = "t3.micro"
+  key_name      = "ecslog"
+  user_data = base64encode(<<-EOF
+      #!/bin/bash
+      echo "ECS_CLUSTER=${aws_ecs_cluster.ecs_cluster.name}" >> /etc/ecs/ecs.config;
+    EOF
+  )
   vpc_security_group_ids = [aws_security_group.elastic_container_security_group.id]
 
   iam_instance_profile {
-    name = aws_iam_instance_profile.ecs_instance_profile.name
+    arn = aws_iam_instance_profile.ecs_instance_profile.arn
+  }
+
+  monitoring {
+    enabled = true
   }
 
   block_device_mappings {
@@ -18,7 +26,6 @@ resource "aws_launch_template" "elastic_launch_template" {
       volume_type = "gp2"
     }
   }
-
   tag_specifications {
     resource_type = "instance"
     tags = {
@@ -61,18 +68,21 @@ resource "aws_key_pair" "elastic_key_pair" {
 }
 
 resource "aws_autoscaling_group" "ecs_autoscaling_group" {
-  desired_capacity = 3
-  max_size         = 4
-  min_size         = 1
+  name_prefix = "elastic_container_asg-"
+  max_size    = 4
+  min_size    = 2
   launch_template {
     id      = aws_launch_template.elastic_launch_template.id
     version = "$Latest"
   }
   vpc_zone_identifier = [for subnet in aws_subnet.elastic_container_subnets : subnet.id if subnet.tags["Name"] == "elastic_container_subnet_public"]
   tag {
-    key                 = "Name"
-    value               = true
+    key                 = "AmazonECSManaged"
+    value               = ""
     propagate_at_launch = true
   }
+
+  # Ensure the ASG is deleted before the launch template
+  depends_on = [ aws_internet_gateway.elastic_container_igw ]
 
 }
